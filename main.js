a828d923892102904f86ee186085cebd1d7b006e
macdaegeon <chleorjs37@gmail.com>
2024-03-17 00:08:15 +0900

    커밋

--- main.js
+++ main.js
@@ -14,4 +14,12 @@ const createWindow = () =>{
 
 app.whenReady().then(()=>{
     createWindow();
+
+    app.on('activate',()=>{
+        if(BrowserWindow.getAllWindows().length === 0) createWindow();
+    })
+})
+
+app.on('window-all-closed',()=>{
+    if(process.platform !== 'darwin') app.quit();
 })
